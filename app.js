446b0e0ccdb429e32db919e5a13d84f1c1cc6dc1
Joakim Waltersson <joakim.waltersson@gmail.com>
2011-07-12 23:28:40 +0200

    Trying to create some classes

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-/**
- * Module dependencies.
- */
-
+// Module dependencies.
 var express = require('express');
 require('jade');
-
 var app = module.exports = express.createServer();
+var gameTracker = require('./lib/gametracker');
 
 // Configuration
-
-app.configure(function(){
+app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -29,51 +25,16 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-var users = [
-  { id: 0, role: 'admin', name: 'Joakim', email: 'joakim.waltersson@gmail.com' }
-  , { id: 1, role: 'guest', name: 'Edvin', email: 'edvin@waltersson.com'}
-];
-
-var User = {
-  find: function(id, fn) {
-    fn(null, users[id]);
-  },
-
-  all: function(fn) {
-    fn(null, users);
-  }
-};
 // Routes
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('index', {
-    title: 'Express'
+    title: 'Zombie dice',
+    games: gameTracker.allGames()
   });
 });
 
-app.get('/socketio', function(req, res){
-  res.render('socketio', {
-    title: 'Socket IO'
-  });
-});
-
-app.get('/user/:id', function(req, res, next){
-  console.log('Show user');
-  User.find(req.params.id, function(err, user){
-    if (!user) return next(new Error('failed to load user'));
-    res.render('users/show', { user: user, title: user.name });
-  })
-});
-
-app.get('/user/:id/edit', function(req, res, next){
-  User.find(req.params.id, function(err, user){
-    if (!user) return next(new Error('failed to load user'));
-    res.render('users/edit', { user: user });
-  })
-});
-
-app.get('/users', function(req, res) {
-  res.render('users', { users: users, title: 'Users' });
+app.get('/roll', function(req, res) {
+  res.send({ dice: 'You rolled ' + Math.floor(Math.random()*11)});
 });
 
 app.listen(8080);
