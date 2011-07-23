673636399c6390220b08b38fc97e3ad2da7aa0cc
Joakim Waltersson <joakim.waltersson@gmail.com>
2011-07-24 00:43:01 +0200

    Re-use id if passed into constructor

--- test/player.test.js
+++ test/player.test.js
@@ -6,5 +6,10 @@ module.exports = {
       var p1 = new Player({ name: 'one'});
       var p2 = new Player({ name: 'two'});
       assert.notEqual(p1.id, p2.id);
+    },
+    'player id should be re-used if present in parameters': function() {
+      var id = '123456';
+      var p = new Player({id: id});
+      assert.equal(p.id, id);
     }
 };
