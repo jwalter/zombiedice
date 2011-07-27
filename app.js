c4fe7d8a1a78b5542b6e0129a0b2da03bc31c58f
Joakim Waltersson <joakim.waltersson@gmail.com>
2011-07-27 16:34:05 +0200

    Nodester port

--- app.js
+++ app.js
@@ -154,5 +154,5 @@ io.sockets.on('connection', function (socket) {
 });
 // Prototype socketio stuff end
 
-app.listen(8080);
+app.listen(11033);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
